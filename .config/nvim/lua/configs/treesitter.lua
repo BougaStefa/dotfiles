af5c040db5c06a4360dd628ff201096de74e4fe8
BougaStefa <bougastefa@gmail.com>
2025-02-01 18:00:25 +0000

    feat(nvim): add nvim config

---
+++ .config/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,22 @@
+local options = {
+  ensure_installed = {
+    "bash",
+    "luadoc",
+    "markdown",
+    "printf",
+    "html",
+    "css",
+    "toml",
+    "vim",
+    "vimdoc",
+    "yaml",
+    "tsx",
+    "java",
+    "xml",
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = { enable = true },
+}
+return options
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = require "configs.treesitter",
+  },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+}
