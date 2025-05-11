ee93779532fe6ef0499e448d8239f3ee25755a82
BougaStefa <bougastefa@gmail.com>
2025-05-11 15:11:17 +0100

    feat: add descriptions to keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,50 +1,49 @@
+local set = vim.keymap.set
+
 -- Set leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- For conciseness
-local opts = { noremap = true, silent = true }
+set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- save file
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+set("n", "<C-s>", "<cmd> w <CR>", { desc = "[S]ave file" })
 
 -- delete single character without copying into register
-vim.keymap.set("n", "x", '"_x', opts)
+set("n", "x", '"_x', { desc = "Delete without copying" })
 
 -- Vertical scroll and center
-vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
-vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 
 -- Find and cecnter
-vim.keymap.set("n", "n", "nzzzv", opts)
-vim.keymap.set("n", "N", "Nzzzv", opts)
+set("n", "n", "nzzzv", { desc = "Find next and center" })
+set("n", "N", "Nzzzv", { desc = "Find previous and center" })
 
 -- Resize with arrows
-vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+set("n", "<Up>", ":resize -2<CR>", { desc = "Reduce current window height" })
+set("n", "<Down>", ":resize +2<CR>", { desc = "Increase current window height" })
+set("n", "<Left>", ":vertical resize -2<CR>", { desc = "Reduce current window width" })
+set("n", "<Right>", ":vertical resize +2<CR>", { desc = "Increase current window width" })
 
 -- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
+set("n", "<leader>v", "<C-w>v", { desc = "[V]ertical split" }) -- split window vertically
+set("n", "<leader>h", "<C-w>s", { desc = "[H]orizontal split" }) -- split window horizontally
+set("n", "<leader>se", "<C-w>=", { desc = "Make [S]plits [E]qual" }) -- make split windows equal width & height
+set("n", "<leader>xs", ":close<CR>", { desc = "E[x]it [S]plit" }) -- close current split window
 
 -- Navigate between splits
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+set("n", "<C-k>", ":wincmd k<CR>", { desc = "Navigate to split above" })
+set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate to split below" })
+set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate to split left" })
+set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate to split right" })
 
 -- Keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP', opts)
+set("v", "p", '"_dP', { desc = "[P]aste without yanking selection" })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+set("n", "<leader>d", vim.diagnostic.open_float, { desc = "[D]iagnostic message" })
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "[Q]uickfix list" })
