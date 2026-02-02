ed5801df6372f244cf0c66b0a5552490a3089526
BougaStefa <BougaStefa@gmail.com>
2026-02-02 22:06:03 +0000

    fix: symlink zshrc, copy over scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,9 +11,10 @@ cp -R "$CONFIG_SRC/"* "$CONFIG_DEST/"
 
 echo "==> Config files copied successfully."
 
-echo "==> Symlinking xprofile and zprofile to home directory..."
+echo "==> Symlinking xprofile, zprofile, and zshrc to home directory..."
 ln -sf "$CONFIG_DEST/x11/xprofile" "$HOME/.xprofile"
 ln -sf "$CONFIG_DEST/shell/profile" "$HOME/.zprofile"
+ln -sf "$CONFIG_DEST/zsh/.zshrc" "$HOME/.zshrc"
 
 echo "==> Setting Zsh as the default shell for $USER..."
 sudo chsh -s /bin/zsh "$USER"
@@ -22,3 +23,10 @@ echo "==> Creating Zsh cache directory..."
 sudo -u "$USER" mkdir -p "/home/$USER/.cache/zsh/"
 
 echo "==> Symlinks created."
+
+echo "==> Copying binaries to ~/.local/bin..."
+mkdir -p "$HOME/.local/bin"
+cp -R "${HOME}/.dotfiles/bin/"* "$HOME/.local/bin/"
+chmod +x "$HOME/.local/bin/"*
+
+echo "==> Binaries and data files copied successfully."
